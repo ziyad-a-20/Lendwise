f132458f26dca6c9ebc2bf7545ba16a1783baca8
ziyad <ziyad.official.a@gmail.com>
2026-06-07 23:42:16 +0530

    Preparing for Deployment

--- app.py
+++ app.py
@@ -66,6 +66,7 @@ pool = MySQLConnectionPool(
     pool_name="lendwise_pool",
     pool_size=5,
     host=os.environ.get('DB_HOST', 'localhost'),
+    port=int(os.environ.get('DB_PORT', 3306)),
     user=os.environ.get('DB_USER', 'root'),
     password=os.environ.get('DB_PASSWORD', ''),
     database=os.environ.get('DB_NAME', 'library_db'),
